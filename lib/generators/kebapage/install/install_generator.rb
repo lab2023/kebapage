cf14c91cc13f770eb1fb0a59f0f43d6cd06939e7
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2014-01-20 21:31:10 +0200

    Add migrations to install generator

--- lib/generators/kebapage/install/install_generator.rb
+++ lib/generators/kebapage/install/install_generator.rb
@@ -3,9 +3,9 @@ require 'rails/generators/base'
 require 'rails/generators/migration'
 require 'rails/generators/active_record'
 
-module Kebapress
+module Kebapage
   module Generators
-    class Kebapage::InstallGenerator < Rails::Generators::NamedBase
+    class Kebapage::InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
       include Rails::Generators::Migration
 
@@ -16,6 +16,13 @@ module Kebapress
 
       def self.next_migration_number(dirname)
         ActiveRecord::Generators::Base.next_migration_number(dirname)
+      end
+
+      def create_migration_file
+        migration_template 'migrations/static_pages.rb', 'db/migrate/create_kebapage_static_pages.rb' rescue nil
+        migration_template 'migrations/photos.rb', 'db/migrate/create_kebapage_photos.rb' rescue nil
+        migration_template 'migrations/add_slug_to_kebapage_static_pages.rb', 'db/migrate/add_slug_to_kebapage_static_pages.rb' rescue nil
+        migration_template 'migrations/friendly_id_slugs.rb', 'db/migrate/create_friendly_id_slugs.rb' rescue nil
       end
 
       def copy_locales
