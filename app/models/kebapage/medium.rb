0edc6c1700275cded118fb97460ef7f30fb966ef
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2014-01-23 14:06:39 +0200

    #5 Move paperclip settings to configs

--- app/models/kebapage/medium.rb
+++ app/models/kebapage/medium.rb
@@ -1,12 +1,12 @@
 module Kebapage
   class Medium < ActiveRecord::Base
     has_attached_file :attachment,
-                      :styles => { large: '640x480#', thumb: '320x240#' },
-                      :url => '/system/kebapage/foto_:id.:extension',
-                      :path => ':rails_root/public:url'
+                      :styles => Rails.configuration.kebapage.attachment_styles,
+                      :url => Rails.configuration.kebapage.attachment_url,
+                      :path => Rails.configuration.kebapage.attachment_path
 
-    validates_attachment_size :attachment, :less_than => 0..512.kilobytes
-    validates_attachment_content_type :attachment, :content_type => %w(image/jpeg image/jpg image/png application/pdf)
+    validates_attachment_size :attachment, :less_than => Rails.configuration.kebapage.attachment_max_size
+    validates_attachment_content_type :attachment, :content_type => Rails.configuration.kebapage.attachment_content_type
 
     Paperclip.interpolates :uniq_file_name do |attachment, style|
       attachment.instance.uniq_file_name
--- lib/kebapage/engine.rb
+++ lib/kebapage/engine.rb
@@ -7,6 +7,11 @@ module Kebapage
     config.after_initialize do
       config.kebapage.front_layout = 'application'
       config.kebapage.admin_layout = 'hq/application'
+      config.kebapage.attachment_styles = { :large => '640x480#', :thumb => '320x240#' }
+      config.kebapage.attachment_url = '/system/kebapage/foto_:id.:extension'
+      config.kebapage.attachment_path = ':rails_root/public:url'
+      config.kebapage.attachment_max_size = 0..512.kilobytes
+      config.kebapage.attachment_content_type = %w(image/jpeg image/jpg image/png application/pdf)
     end
   end
 end
